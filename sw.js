2fdebd327fbf3b0b3a00b765c3cd2ebdcd32fa6f
jjiyeokok-rgb <jjiyeokok@gmail.com>
2026-08-02 21:36:44 +0900

    낭독스쿨 체크 항목 수정 (#6)
    Co-authored-by: jjiyeokok-rgb <jjiyeokok-rgb@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "siyeon-mission-v7";
+const CACHE_NAME = "siyeon-mission-v8";
 const APP_FILES = ["./", "./index.html", "./style.css", "./app.js", "./cloud-config.js", "./cloud-sync.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
