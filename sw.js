2fa925639c241b4174f0954f6f17b8b6e167394a
jjiyeokok-rgb <jjiyeokok@gmail.com>
2026-08-02 20:50:56 +0900

    모바일 줄바꿈과 상단 배치 개선
    Co-authored-by: jjiyeokok-rgb <jjiyeokok-rgb@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "siyeon-mission-v3";
+const CACHE_NAME = "siyeon-mission-v4";
 const APP_FILES = ["./", "./index.html", "./style.css", "./app.js", "./cloud-config.js", "./cloud-sync.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
